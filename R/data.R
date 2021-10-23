a87773d618584b704ff64f13c2084f9ae660f109
Yang Liu <lylyf1987@gmail.com>
2021-10-22 20:58:19 -0500

    added real data into package

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Real data
+#'
+#' Summarized real data from this study:
+#' Empirical assessment of analysis workflows for differential expression analysis using RNA-Seq.
+#' This study has totally 34 samples, 17 classical and 17 nonclassical
+#'
+#' @format A data frame with 605015 rows and 36 variables:
+#' \describe{
+#'   \item{read_gene}{gene id}
+#'   \item{read_type}{read type id}
+#'   \item{group0_sample1}{number of read type for sample1 in group0}
+#'   \item{group1_sample1}{number of read type for sample1 in group1}
+#'   ...
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/sra?linkname=bioproject_sra_all&from_uid=339968}
+"real_data"
